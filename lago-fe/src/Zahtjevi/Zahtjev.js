f62d08eca197d689abadc1d207f0a057aa9f6a66
Šeila Bećirović <seila.becirovic@gmail.com>
2018-05-18 12:50:38 +0200

    Detalji o korisniku

--- lago-fe/src/Zahtjevi/Zahtjev.js
+++ lago-fe/src/Zahtjevi/Zahtjev.js
@@ -30,7 +30,7 @@ class Zahtjev extends Component{
             id:111,
             naziv:'Naziv zahtjeva',
             korisnik:'Korisnik Korisniković',
-            rjesava: 'Korisnik Korisniković',
+            rjesava: null,
             opis:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
             hitnost:2,
             status:'U obradi',
@@ -38,7 +38,7 @@ class Zahtjev extends Component{
             datumprijave:'11/11/11',
             datumrjesenja:'N/A',
             odjel: 'Odjel 1',
-            eskalacija: 'Ne'
+            eskalacija: 'Da'
         };
         var poruke = [{
           autor: "Admin", datum:"11/11/11", tekst:"Nesto"
